b970cb9ae16bf7f45e6e7f7d4b174345962e20a7
Joy <joydong@berkeley.edu>
2020-10-12 02:35:45 -0700

    css is rough

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Button, { ButtonTextType, ButtonType } from "./components/button/button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Button
+                    text="LOG IN"
+                    buttonType={ButtonType.CLEAR}
+                    textType={ButtonTextType.WHITE}
+                    onPress={() => console.log("I'm the first button.")}
+                />
+                <Button
+                    text="CONTINUE"
+                    buttonType={ButtonType.GREEN}
+                    textType={ButtonTextType.WHITE}
+                    onPress={() => console.log("I'm the second.")}
+                />
+                <Button
+                    text="SKIP"
+                    buttonType={ButtonType.WHITE}
+                    textType={ButtonTextType.GREY}
+                    onPress={() => console.log("Third.")}
+                />
+                <Button
+                    text="ONCE A DAY"
+                    buttonType={ButtonType.CLEAR_ALT}
+                    textType={ButtonTextType.GREEN}
+                    onPress={() => console.log("Hello! I'm the last button!")}
+                />
+            </header>
+        </div>
+    );
 }
 
 export default App;
